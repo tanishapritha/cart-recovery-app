9e8c86e18e7969d4f88fea9d636fd5d3f5523f7e
tanishapritha <tpritha1903@gmail.com>
2025-06-26 01:45:40 +0530

    .

--- cart-recovery-frontend/vite.config.js
+++ cart-recovery-frontend/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    base: process.env.VITE_BASE_PATH || "/cart-recovery-app"
-
-  ]
+  base: '/', // ✅ use '/' for root deployment on Vercel
+  plugins: [react()],
 })
